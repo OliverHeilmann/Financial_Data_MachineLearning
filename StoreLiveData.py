f145f685eafa75e0a3d5fca44e0baad56d95d6d8
Oliver Heilmann <OliverHeilmann@Olivers-MBP.lan>
2020-03-15 20:15:26 +0000

    final prep

--- StoreLiveData.py
+++ StoreLiveData.py
@@ -82,16 +82,15 @@ def stockmarket_openhours():
     hour = datetime.now(USA_East).hour
     
     # Simple logic to stop collecting data during market close
-#    if 4 <= weekday <= 6:
-#        if weekday == 4 and hour <= 17:
-#            return True
-#        elif weekday == 6 and hour >= 18:
-#            return True
-#        else:
-#            return False
-#    else:
-#        return True
-    return True
+    if 4 <= weekday <= 6:
+        if weekday == 4 and hour <= 17:
+            return True
+        elif weekday == 6 and hour >= 18:
+            return True
+        else:
+            return False
+    else:
+        return True
 
           
 # Main code with governing parameters
@@ -99,10 +98,10 @@ if __name__ == '__main__':
     # Required Parameters
     trigger = False     # trigger to stop collecting data on market close
     tickercolumn = 0    # which column are tickers in
-    ticker_size = 10    # number of tickers used from Wiki URL
+    ticker_size = 100   # number of tickers used from Wiki URL
     threads = 5         # number of threads pulling ticker data
-    pull_step = 1      # time (seconds) between price pull
-    rows = 10           # number of rows before csv is pushed to Github (1 hour)
+    pull_step = 60      # time (seconds) between price pull
+    rows = 60           # number of rows before csv is pushed to Github (1 hour)
     
     # Minute by Minute filename
     m_by_m = 'minute_by_minute.csv'

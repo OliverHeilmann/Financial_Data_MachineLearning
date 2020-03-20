5ee62c47dcf145663514b6b686ee8f6810c6ad60
Oliver Heilmann <OliverHeilmann@Olivers-MBP.lan>
2020-03-20 12:33:07 +0000

    added

--- StoreLiveData.py
+++ StoreLiveData.py
@@ -113,7 +113,7 @@ def stockmarket_openhours(tmzone, m_open, m_close):
 if __name__ == '__main__':
     ############## MANUAL PARAMETERS REQUIRED TO BE SET BELOW ################
     tickercolumn = 1    # which column are tickers in
-    ticker_size = 150   # number of tickers used from Wiki URL
+    ticker_size = 10   # number of tickers used from Wiki URL
     threads = 10        # number of threads pulling ticker data
     pull_step = 1      # time (60 seconds) between price pull
     rows = 10           # number of rows before csv is pushed to Github (1 hour)
